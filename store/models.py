58c158ed27dcca2aaedeb15ace82b250d90d5762
Carlos Ceron <carlos.ceron.1986@gmail.com>
2021-04-14 00:13:31 -0500

    Configuracion admin y carpetas multimedia.

--- store/models.py
+++ store/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Category(models.Model):
 	name = models.CharField(max_length=255, db_index=True)
 	slug = models.SlugField(max_length=255, unique=True)
