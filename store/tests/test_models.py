c2bee89560dd0babcd2c0e4ce2160cbf3cf8f60a
Carlos Ceron <carlos.ceron.1986@gmail.com>
2021-04-17 23:15:30 -0500

    Views y urls.

--- store/tests/test_models.py
+++ store/tests/test_models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
 from store.models import Category, Product
 
@@ -20,3 +21,18 @@ class TestCategoriesModel(TestCase):
 		"""
 		data = self.data1
 		self.assertEqual(str(data), "django")
+
+class TestProductModel(TestCase):
+	
+	def setUp(self) -> None:
+		Category.objects.create(name="django", slug="django")
+		User.objects.create(username="admin",)
+		self.data1 = Product.objects.create(category_id=1, title="django beginners", created_by_id=1, slug="django-beginner", price="20.00", image="django")
+	
+	def test_product_models_entry(self):
+		"""
+		Test Product model return the model name
+		"""
+		data = self.data1
+		self.assertTrue(isinstance(data, Product))
+		self.assertEqual(str(data), "django beginners")
