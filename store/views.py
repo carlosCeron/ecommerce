44fb216f1744efc08a0e6e70f0b305847160d131
Carlos Ceron <carlos.ceron.1986@gmail.com>
2021-04-18 15:06:12 -0500

    Template base y categorias.

--- store/views.py
+++ store/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from .models import Category, Product
 
 
+def categories(request):
+	categories_list = Category.objects.all()
+	return {'categories': categories_list}
+
+
 def all_products(request):
 	products = Product.objects.all()
 	return render(request, 'store/home.html', {'products': products})
